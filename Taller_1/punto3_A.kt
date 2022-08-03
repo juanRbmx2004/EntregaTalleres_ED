b4153549ec0b94b994d0fa8643cb751a8ade526e
Juan Sebastian Ruiz Fernandez <101655382+juanRbmx2004@users.noreply.github.com>
2022-08-02 23:56:24 -0500

    Add files via upload

---
+++ Taller_1/punto3_A.kt
@@ -0,0 +1,27 @@
+package Taller_1
+
+/*ENTRADAS ---> Base-->B--->double
+           ---> Altura--->A--->double
+
+Salidas ----> Area Del Triangulo -->At--->double
+
+*/
+/*
+Realice una función que halle el área de un triángulo.
+ */
+
+fun MedidasTriangulo(B:Double,A:Double):Double{
+
+    var At = B*A/2
+
+    return At
+}
+fun main(){
+
+    print("ingrese la altura del triangulo: ")
+    val A = readLine()!!.toDouble()
+    print("ingrese la base del triagulo: ")
+    val B = readLine()!!.toDouble()
+
+    println("\nEl area del triangulo es de: "+ MedidasTriangulo(A,B))
+}
